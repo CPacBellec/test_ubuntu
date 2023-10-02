80eb56ce5efaf8c8c9cec40ab41e1aa9e7d4b1ca
Cyril Bellec <cbellec@normandiewebschool.fr>
2023-10-02 22:20:48 +0200

    new commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Accueil - MonSiteCritique</title>
+    <!-- Ajoutez ici vos fichiers CSS et JavaScript -->
+</head>
+<body>
+    <header>
+        <h1>MonSiteCritique</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Films</a></li>
+                <li><a href="#">Jeux vidéo</a></li>
+                <li><a href="#">Livres</a></li>
+                <li><a href="#">Séries télévisées</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    
+    <section>
+        <h2>Jeux vidéo populaires</h2>
+        <!-- Affichez ici les jeux vidéo populaires depuis la base de données -->
+    </section>
+    
+    <section>
+        <h2>Jeux récents</h2>
+        <!-- Affichez ici les livres populaires depuis la base de données -->
+    </section>
+    
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> MonSiteCritique</p>
+    </footer>
+</body>
+</html>
